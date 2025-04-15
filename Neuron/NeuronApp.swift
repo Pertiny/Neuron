e7a4ed29abb4bcb49cd51c64af901b7fade5b0a7
Pertiny <jacques.mz@yahoo.com>
2025-04-15 16:43:30 +0200

    Initial Commit

---
+++ Neuron/NeuronApp.swift
@@ -0,0 +1,17 @@
+//
+//  NeuronApp.swift
+//  Neuron
+//
+//  Created by Jacques Zimmer on 15.04.25.
+//
+
+import SwiftUI
+
+@main
+struct NeuronApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
